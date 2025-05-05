02892c7758d0a77e947dc74f3180d08da6055117
Dilu Kefale <dilu.kefale@gmail.com>
2025-05-05 13:09:25 +0300

    rm import

--- src/lib/db/migrate.ts
+++ src/lib/db/migrate.ts
@@ -1,7 +1,6 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { migrate } from "drizzle-orm/neon-http/migrator";
 import { neon } from "@neondatabase/serverless";
-import * as schema from "./schema";
 
 async function main() {
   const sql = neon(process.env.DATABASE_URL!);
