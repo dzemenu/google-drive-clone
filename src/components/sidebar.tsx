71c4f1b6879486a4d8fe5ad94ed93aee410c8e95
Dilu Kefale <dilu.kefale@gmail.com>
2025-05-06 10:45:26 +0300

    fix bulid error

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,4 +1,4 @@
-import { Folder, Trash, LogOut, Plus, Upload, Menu } from "lucide-react";
+import { LogOut, Plus, Upload, Menu } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { useClerk } from "@clerk/nextjs";
 
@@ -23,9 +23,8 @@ export function Sidebar({ onAddFolder, onUpload, isOpen, onClose }: SidebarProps
       </button>
 
       {/* Sidebar */}
-      <aside className={`fixed md:static inset-y-0 left-0 z-40 w-64 bg-white dark:bg-gray-800 shadow-md p-4 space-y-4 transform transition-transform duration-200 ease-in-out ${
-        isOpen ? 'translate-x-0' : '-translate-x-full md:translate-x-0'
-      }`}>
+      <aside className={`fixed md:static inset-y-0 left-0 z-40 w-64 bg-white dark:bg-gray-800 shadow-md p-4 space-y-4 transform transition-transform duration-200 ease-in-out ${isOpen ? 'translate-x-0' : '-translate-x-full md:translate-x-0'
+        }`}>
         <div className="flex flex-col gap-4 mt-8">
           <Button onClick={onAddFolder} className="flex gap-2 w-full justify-center items-center">
             <Plus size={18} /> Add Folder
